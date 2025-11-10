5e6a35b722d42642eede6ca5d5117a660d120e76
LarsAtassi <44405262+LarsAtassi@users.noreply.github.com>
2025-11-10 16:01:48 +0100

    chg terraform files (simplified) WIP

--- variables.tf
+++ variables.tf
@@ -1,2 +1,11 @@
-variable "region" { type = string, default = "us-east-1" }
-variable "instance_type" { type = string, default = "t3.micro" }
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
